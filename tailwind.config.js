419ac9e59280185d7a8251309b539c3f1040fd7e
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-01-22 14:46:24 +0700

    add: setup flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}", "./public/**/*.{html,js}"],
+  content: [
+    './src/**/*.{js,jsx}', 
+    './public/**/*.{html,js}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
